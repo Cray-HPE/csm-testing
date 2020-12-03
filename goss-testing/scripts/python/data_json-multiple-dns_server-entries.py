1d85f133620af7576ee5faeaceee52b74c8fbe8d
Mark Winnemueller <mark.winnemueller@hpe.com>
2020-12-03 13:53:54 -0700

    Added /scripts/python directory. /scripts/python/lib directory. Added module to parse data.json and a script, using that module. Created a gosss test for casminst-384 to check for multiple instance of dns-server. tested.

--- goss-testing/scripts/python/data_json-multiple-dns_server-entries.py
+++ goss-testing/scripts/python/data_json-multiple-dns_server-entries.py
@@ -5,15 +5,12 @@ import sys
 import lib.data_json_parser as dp
 
 if __name__ == '__main__':
-    if os.path.isfile('/mnt/configs/data.json'):
-        data_file = '/mnt/configs/data.json'
-    elif os.path.isfile('/var/www/ephemeral/configs/data.json'):
-        data_file = '/var/www/ephemeral/configs/data.json'
-    else:
-        sys.exit("Could not find data.json")
+    # where is data.json?
+    dataFile = '/mnt/configs/data.json'
+    if len(sys.argv) > 1:
+        dataFile = sys.argv[1]
 
-
-    dj = dp.dataJson()
+    dj = dp.dataJson(dataFile)
     glbal = dj.payload['Global']['meta-data']
 
     count = 0
