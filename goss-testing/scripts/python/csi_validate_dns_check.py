a152dc03517a4e4a0ffa5caa6ac837fd5533024f
Mark Winnemueller <mark.winnemueller@hpe.com>
2020-12-15 13:23:18 -0700

    Adding in csi pit validate dns test. The goss script and accomanying python script gets the list of ncns from data.jason and ensures that they are in dnsmasq.leases. Very similar to what csi pit validate dns does, except we check against data.json instead of just printing them out

---
+++ goss-testing/scripts/python/csi_validate_dns_check.py
@@ -0,0 +1,37 @@
+import sys, os
+import lib.data_json_parser as dp
+
+''' Simple test to validate dns. It checks the output of `grep -Eo 'ncn-.*-mgmt'` against data.json global meta-data ntp-peers
+Counts the number of times that the ntp-peer appears in the output of the grep
+matches that to the number of ntp_peers - they should be ==
+'''
+
+PASSED = 0
+dnsmasq_file = '/var/lib/misc/dnsmasq.leases'
+
+# Assume we got the right info from goss, but JIC
+if len(sys.argv) == 2:
+    data_json = sys.argv[1]
+else:
+    data_json = "/var/www/ephemeral/configs/data.json"
+
+# load the info from dnsmasq.leases
+if os.path.isfile(dnsmasq_file):
+    f = open(dnsmasq_file, 'r')
+    dns_contents = f.read()
+
+dj = dp.dataJson(data_json)
+peers = dj.getGlobalMD()["ntp_peers"].split()
+
+# If this machine(ncn-m001) is in data.json global meta-data ntp-peers, remove it
+if 'ncn-m001' in peers:
+    peers.remove('ncn-m001')
+
+for peer in peers:
+    if peer+'-mgmt' in dns_contents:
+        PASSED += 1
+
+if PASSED == len(peers):
+    print("PASS")
+else:
+    print("FAIL")
