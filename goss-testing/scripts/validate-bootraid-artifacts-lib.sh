e2f686975257eb943c71f8b9d91a20ab72bf4131
Jacob Salmela <jacob.salmela@hpe.com>
2021-10-19 14:44:28 -0500

    Revert "Merge pull request #78 from Cray-HPE/CASMINST-3349"
    This reverts commit d1ab85f35afca2ce54741274570ddb3219b24c40, reversing
    changes made to 62e705f72665093de016b73213c5e35767e4f05b.

--- goss-testing/scripts/validate-bootraid-artifacts-lib.sh
+++ goss-testing/scripts/validate-bootraid-artifacts-lib.sh
@@ -2,9 +2,9 @@
 set -eu
 set -o pipefail
 
-ARTIFACT_S3_PATH="${1}"
+ARTIFACT_PATH="${1}"
 
-if [[ -z ${ARTIFACT_S3_PATH} ]]; then
+if [[ -z ${ARTIFACT_PATH} ]]; then
 	echo "Path to a kernel or initrd in S3 is required as an argument"
 	# shellcheck disable=SC2154 # this is just an example command
 	echo "Try 'cray bss bootparameters list --hosts ${xname} | awk '/kernel = /'"
@@ -33,7 +33,7 @@ fi
 cleanup() {
 	if ! eval pit_check; then
 		if eval mount | grep "${BOOTRAID}" >/dev/null; then
-			umount "${BOOTRAID}"
+			umount $BOOTRAID
 		fi
 	fi
 
@@ -54,7 +54,6 @@ mount_bootraid() {
 }
 
 expected_initrd_name() {
-  local expected_name=""
 	# find the name the grub config expects
 	expected_name=$(grep initrdefi "${BOOTRAID}"/boot/grub2/grub.cfg | awk '{print $2}' | awk -F'/' '{print $NF}')
 	echo "${expected_name}"
@@ -63,34 +62,21 @@ expected_initrd_name() {
 get_artifact_from_s3() {
 	local artifact="${1}"
 	local path="${2}"
-  local liveos=/run/initramfs/live/LiveOS
 
 	mount_bootraid
 
-	echo "Getting $(basename "${artifact}") from s3 (${path})..."
-	curl -s -o "${artifact}" http://rgw-vip.nmn/"${path/s3:\/\/}"
-
-  if ! check_boot_artifact "${artifact}"; then
-    echo "${artifact} is not a valid file type.  Is S3 working?"
-    echo "Copying local artifacts from ${liveos}/$(basename "${artifact}")"
-
-    if ! cp "${liveos}/$(basename "${artifact}")" "${artifact}"; then
-      echo "Unable to find a valid artifact"
-      exit 1
-    fi
-  
-  fi
+	echo "Getting $(basename ${artifact}) from s3 (${path})..."
+	curl -s -o ${artifact} http://rgw-vip.nmn/"${path/s3:\/\/}"
 
 	REBOOT_NEEDED="true"
 }
 
 check_boot_artifact() {
 	local artifact="$1"
-	# check if the filesize is bigger than 92 or 217 (that is the size of an XML error message from s3)
+	# check if the filesize is bigger than 217 (that is the size of an XML error message from s3)
 	# or check if this is a filetype we expect for a kernel or initrd
-	if ! [[ $(stat --format=%s "${artifact}") -gt 92 ]] || \
-    ! [[ $(stat --format=%s "${artifact}") -gt 217 ]] ||
-    ! [[ $(file --brief --mime-type "${artifact}") == application/octet-stream ]]; then
+	if ! [[ "$(stat --format=%s $f)" -gt 217 ]] || ! [[ "$(file --brief --mime-type $f)" == application/octet-stream ]]; then
+		echo -e "\n${f} size is too small or not an expected file type ($(stat --format=%s $f):$(file --brief --mime-type $f))"
 		return 1
 	else
 		return 0
@@ -100,12 +86,12 @@ check_boot_artifact() {
 # mount the raid
 mount_bootraid
 
-if [[ "${ARTIFACT_S3_PATH}" == *kernel* ]]; then
+if [[ "${ARTIFACT_PATH}" == *kernel* ]]; then
 	printf "Examining kernel..."
 	f="${BOOTRAID}/boot/kernel"
-elif [[ "${ARTIFACT_S3_PATH}" == *initrd* ]]; then
+elif [[ "${ARTIFACT_PATH}" == *initrd* ]]; then
 	printf "Examining initrd..."
-	f="${BOOTRAID}/boot/$(expected_initrd_name)"
+	f="${BOOTRAID}/boot/initrd.img.xz"
 else
 	printf "Unknown artifact for this script."
 	exit 1
@@ -118,35 +104,32 @@ if [[ -f "${f}" ]]; then
 	# if the file is bad
 	if ! eval check_boot_artifact "${f}"; then
 
-    echo -e "\n${f} size is too small or not an expected file type ($(stat --format=%s "${f}"):$(file --brief --mime-type "${f}"))"
-
-    echo "Removing faulty file (${f})"
 		# remove the problematic file
 		rm -f "${f}"
 
 		# then get a known-good artifact from the path discovered in s3
-		get_artifact_from_s3 "${f}" "${ARTIFACT_S3_PATH}"
+		get_artifact_from_s3 "${f}" "${ARTIFACT_PATH}"
 
-		if [[ $(basename "${f}") == *initrd* ]]; then
+		if [[ $(basename ${f}) == *initrd* ]]; then
 			# Modify the filename if grub is expecting something else
 			# Sometimes it was named initrd, other times it has been initrd.img.xz
-			if [[ $(basename "${f}") != $(expected_initrd_name) ]]; then
-				echo mv "${f}" "$(dirname "${f}")/$(expected_initrd_name)"
+			if [[ $(basename ${f}) != $(expected_initrd_name) ]]; then
+				echo mv "${f}" "$(dirname ${f})/$(expected_initrd_name)"
 			fi
 
 		fi
 
 	else
 
-		printf '%s is OK.\n' "$(basename "${f}")"
+		printf "$(basename ${f}) is OK.\n"
 
 	fi
 
 else
 
-	echo '%s not found in BOOTRAID.' "$(basename "${f}")"
+	echo "$(basename ${f}) not found in BOOTRAID."
 
-	get_artifact_from_s3 "${f}" "${ARTIFACT_S3_PATH}"
+	get_artifact_from_s3 "${f}" "${ARTIFACT_PATH}"
 
 fi
 
