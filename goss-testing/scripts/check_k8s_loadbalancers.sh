e176102dfb95f2afe01b5ea0e8367509e25d317f
Dat Le <dle@cray.com>
2022-06-09 15:31:39 -0500

    update test check_k8s_loadbalancers.sh to test when there is no CMN

--- goss-testing/scripts/check_k8s_loadbalancers.sh
+++ goss-testing/scripts/check_k8s_loadbalancers.sh
@@ -44,7 +44,8 @@ do
     esac
 done
 
-function get_default_net_from_sls() {
+function get_sls_network_data() {
+
   export TOKEN=$(curl -s -k -S -d grant_type=client_credentials -d client_id=admin-client -d client_secret=`kubectl get secrets admin-client-auth -o jsonpath='{.data.client-secret}' | base64 -d` https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token | jq -r '.access_token')
 
   NETWORKSJSON=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" https://api-gw-service-nmn.local/apis/sls/v1/networks)
@@ -53,23 +54,61 @@ function get_default_net_from_sls() {
       echo >&2 "error: failed to get Networks from SLS"
       exit 1
   fi
-
-  defaultRoute=$(echo "${NETWORKSJSON}" | jq -r --arg n "BICAN" '.[] | select(.Name == $n) | .ExtraProperties | .SystemDefaultRoute')
-  echo "$defaultRoute"
+  echo "$NETWORKSJSON"
 }
 
-default_bican_net=$(get_default_net_from_sls)
+function check_chn_config_from_sls() {
 
-# Check to see if the CHN is configured by checking the BICAN toggle in SLS
-chn_configured=0
-if [ "$default_bican_net" == "CHN" ]; then
+  defaultRoute=$(echo "$1" | jq -r --arg n "BICAN" '.[] | select(.Name == $n) | .ExtraProperties | .SystemDefaultRoute')
+
+  # Check to see if the CHN is configured by checking the BICAN toggle in SLS
+  chn_configured=0
+  if [ "$defaultRoute" == "CHN" ]; then
     chn_configured=1
-else
+  else
     chn_configured=0
-fi
+  fi
 
-if [[ ${chn_configured} -eq 1 ]]
-# CHN is configured
+  echo "$chn_configured"
+}
+
+function check_cmn_config_from_sls() {
+
+  cmn_config=$(echo "$1" | jq -r --arg n 'customer-management-static' '.[].ExtraProperties.Subnets[]| select(.MetalLBPoolName == $n)')
+
+  # Check to see if the CHN is configured by checking the BICAN toggle in SLS
+  cmn_configured=0
+  if [[ -n "$cmn_config" ]]; then
+    cmn_configured=1
+  else
+    cmn_configured=0
+  fi
+
+  echo "$cmn_configured"
+}
+
+function check_can_config_from_sls() {
+
+  can_config=$(echo "$1" | jq -r --arg n "can_metallb_static_pool" '.[].ExtraProperties.Subnets[]| select(.Name == $n)')
+
+  # Check to see if the CHN is configured by checking the BICAN toggle in SLS
+  can_configured=0
+  if [[ -n "$can_config" ]]; then
+    can_configured=1
+  else
+    can_configured=0
+  fi
+
+  echo "$can_configured"
+}
+
+networks_json=$(get_sls_network_data)
+chn_sls_check=$(check_chn_config_from_sls "$networks_json")
+cmn_sls_check=$(check_cmn_config_from_sls "$networks_json")
+can_sls_check=$(check_can_config_from_sls "$networks_json")
+
+if [[ ${chn_sls_check} -eq 1 ]]
+# CHN is configured and CMN is configured
 then
     if [[ ${print_results} -eq 1 ]]
     then
@@ -87,14 +126,35 @@ then
             if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo 'PASS'; else echo 'FAIL'; fi
         fi
     done
-else
-# CHN is not configured
+elif [[ ${chn_sls_check} -eq 0 && ${cmn_sls_check} -eq 1 ]]
+then
+# CHN is not configured and CMN is configured
     if [[ ${print_results} -eq 1 ]]
     then
         echo 'INFO: CHN is not configured, excluding istio-ingressgateway-chn and cray-oauth2-proxies-customer-high-speed-ingress LoadBalancers'
+        echo 'INFO: CMN is configured, including CMN LoadBalancers'
         printf '\n%-16s %-35s %-15s %-4s\n' "NAMESPACE" "LOADBALANCER" "IPADDRESS" "STATUS"
     fi
     kubectl get service -A | grep LoadBalancer | grep -Ev "istio-ingressgateway-chn|cray-oauth2-proxies-customer-high-speed-ingress" | awk '{print $1" "$2" "$5}' | \
+    while read namespace loadbalancer ip
+    do
+        if [[ ${print_results} -eq 1 ]]
+        then
+            if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then status='PASS'; else status='*FAIL*'; fi
+            printf '%-16s %-35s %-15s %-4s\n' ${namespace} ${loadbalancer} ${ip} "${status}"
+        else
+            if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then echo 'PASS'; else echo 'FAIL'; fi
+        fi
+    done
+else
+# CHN is not configured and CAN is configured
+    if [[ ${print_results} -eq 1 ]]
+    then
+        echo 'INFO: CHN is not configured, excluding istio-ingressgateway-chn and cray-oauth2-proxies-customer-high-speed-ingress LoadBalancers'
+        echo 'INFO: CAN is configured, including CAN LoadBalancers'
+        printf '\n%-16s %-35s %-15s %-4s\n' "NAMESPACE" "LOADBALANCER" "IPADDRESS" "STATUS"
+    fi
+    kubectl get service -A | grep LoadBalancer | grep -Ev "istio-ingressgateway-chn|cray-oauth2-proxies-customer-high-speed-ingress|cmn" | awk '{print $1" "$2" "$5}' | \
     while read namespace loadbalancer ip
     do
         if [[ ${print_results} -eq 1 ]]
