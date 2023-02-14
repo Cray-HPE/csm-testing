608b6fb3e5f229429472ba14b0bdfabee13c1379
dle-hpe <80925073+dle-hpe@users.noreply.github.com>
2023-02-14 14:49:56 -0600

    CASMINST-5908-csm-1.4 - updated check_bgp_neighbors.sh to load switch admin password from vault (#437)
    * updated check_bgp_neighbors.sh to load switch admin password from vault
    
    * fix error output syntax
    
    * add logic to handle test scenarios before and after vault is installed

--- goss-testing/scripts/check_bgp_neighbors_established.sh
+++ goss-testing/scripts/check_bgp_neighbors_established.sh
@@ -48,6 +48,14 @@ SECRET=$(kubectl get secrets admin-client-auth -o jsonpath='{.data.client-secret
 TOKEN=$(curl -s -k -S -d grant_type=client_credentials -d client_id=admin-client -d client_secret="$SECRET" https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token | jq -r '.access_token') ||
     err_exit 15 "Command pipeline failed with return code $?: curl -s -k -S -d grant_type=client_credentials -d client_id=admin-client -d client_secret=XXXXXX https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token | jq -r '.access_token'"
 
+# check if vault configmap exists to verify vault has been installed
+# Set vault_check to 1 if we cannot get the Vault cray-vault-configurator configmap.  Set to 0 otherwise.
+if kubectl -n vault get cm cray-vault-configurer ; then
+    vault_check=0
+else
+    vault_check=1
+fi
+
 # check if metalLB configmap exists
 # Set metallb_check to 1 if we cannot get the metallb configmap from Kubernetes. Set to 0 otherwise.
 if kubectl -n metallb-system get cm metallb ; then
@@ -64,7 +72,17 @@ curl -s -k -H "Authorization: Bearer ${TOKEN}" https://api-gw-service-nmn.local/
 # has the CAN or CHN strings in its ExtraProperties.SystemDefaultRoute field. Otherwise set to 1
 [[ $? -eq 0 ]] && sls_network_check=0 || sls_network_check=1
 
-if [ -z "$SW_ADMIN_PASSWORD" ]; then
+# vault is running and checking for switch admin password in vault
+if [ -z "$SW_ADMIN_PASSWORD" ] && [ "$vault_check" -lt "1" ]; then
+    VAULT_PASSWD=$(kubectl -n vault get secrets cray-vault-unseal-keys -o json | jq -r '.data["vault-root"]' |  base64 -d)
+    SW_ADMIN_PASSWORD=$(kubectl -n vault exec -i cray-vault-0 -c vault -- env VAULT_TOKEN="$VAULT_PASSWD" VAULT_ADDR=http://127.0.0.1:8200 VAULT_FORMAT=json vault kv get secret/net-creds/switch_admin | jq -r  .data.admin) ||
+        err_exit 20 'Detected Vault is running.  Missing switch admin password from vault.  Please run the following commands:
+        VAULT_PASSWD=$(kubectl -n vault get secrets cray-vault-unseal-keys -o json | jq -r '.data["vault-root"]' |  base64 -d)
+        alias vault='kubectl -n vault exec -i cray-vault-0 -c vault -- env VAULT_TOKEN='"$VAULT_PASSWD"' VAULT_ADDR=http://127.0.0.1:8200 VAULT_FORMAT=json vault'
+        vault kv put secret/net-creds/switch_admin admin=SWITCH_ADMIN_PASSWORD'
+fi
+# this is the case if vault is not running and env variable has not been set
+if [ -z "$SW_ADMIN_PASSWORD" ] && [ "$vault_check" -gt "0" ]; then
     echo "******************************************"
     echo "******************************************"
     echo "**** Enter SSH password of switches: ****"
