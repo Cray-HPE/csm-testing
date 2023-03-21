c52007491b13f6be5a51d011054ed13c02ac3605
kimjensen-hpe <77168453+kimjensen-hpe@users.noreply.github.com>
2023-03-21 10:00:50 -0600

    CASMPET-6429 1.5 : csm-vshasta-deploy test failure : k8s_postgres_pods_per_cluster (#469)

--- goss-testing/scripts/postgres_pods_running.sh
+++ goss-testing/scripts/postgres_pods_running.sh
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2022-2023 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -22,6 +22,14 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
+# How many postgres pods should be running?
+#  Do no hard code the number of instances -- determine from postgresql, customizations and/or patronictl list.
+#    - Fail if customizations has set sqlCluster.instanceCount, and the postgresql numberOfInstances is not the same as the
+#           customizations sqlCluster.instanceCount (no clear source of truth).
+#    - Fail if the number of Running pods for the postgres cluster is not the same as the postgresql numberOfInstances.
+#    - Fail if the number of running cluster members (as reported by patronictl list) is not the same as the postgresql 
+#           numberOfInstances.
+
 print_results=0
 while getopts ph stack
 do
@@ -44,45 +52,52 @@ do
     c_ns="$(echo $c | awk -F, '{print $1;}')"
     c_name="$(echo $c | awk -F, '{print $2;}')"
 
-    num_pods_running=$(kubectl get pods -n $c_ns -l "application=spilo,cluster-name=${c_name}" | grep Running | wc -l)
-    clusterFailFlag=0
-    if [[ $c_name == "sma-postgres-cluster" ]]; then
-        if [[ $num_pods_running -ne 2 ]]; then clusterFailFlag=1; fi
-    else
-        if [[ $num_pods_running -ne 3 ]]; then clusterFailFlag=1; fi
+    # Determine the number of postgres pods based on the numberIfInstances from the postgresql cr and the site-init customizations sqlCluster.instanceCount.
+    # If the sqlCluster.instanceCount is set in the customizations and it is not the same as that set in the postgresql cr then fails and continue to the next cluster.
+    num_of_instances=$(kubectl get postgresql -n $c_ns ${c_name} -o json | jq -r '.spec.numberOfInstances')
+    service_name=$(kubectl get postgresql -n $c_ns ${c_name} -o yaml | grep "meta.helm.sh/release-name:" | awk '{print $2}')
+    customization_instance_count=$(kubectl get secrets -n loftsman site-init -o jsonpath='{.data.customizations\.yaml}' | base64 -d | yq read customizations.yaml "spec.kubernetes.services.${service_name}.cray-service*.sqlCluster.instanceCount")
+
+    if [[ ! -z $customization_instance_count ]]; then
+        if [[ $num_of_instances -ne $customization_instance_count ]]; then
+            failFlag=1;
+	    if [[ $print_results -eq 1 ]]
+	    then
+		echo "$c_name -- Postgresql numOfInstances:$num_of_instances and sqlCluster.instanceCount:$customization_instance_count do not match (fail)"
+            fi
+            continue
+	fi
     fi
 
-    if [[ $clusterFailFlag -eq 1 ]]
-    then
+    num_pods_running=$(kubectl get pods -n $c_ns -l "application=spilo,cluster-name=${c_name}" | grep Running | wc -l)
+    if [[ $num_pods_running -ne $num_of_instances ]]; then 
+	failFlag=1
         if [[ $print_results -eq 1 ]]
         then
-            echo "Error: $c_name does not have the expected number of pods Running."
+            echo "$c_name -- Does not have the expected number of $num_pods_running pods Running (fail)"
             kubectl get pods -n $c_ns -l "application=spilo,cluster-name=${c_name}"
             echo
-            failFlag=1;
-        else exit 1; fi
+	fi
+        continue
     fi
 
-    clusterFailFlag=0
     first_member="$(kubectl get pod -n $c_ns -l "cluster-name=$c_name,application=spilo" \
                   -o custom-columns=NAME:.metadata.name --no-headers | head -1)"
     num_patronictl_running=$(kubectl -n $c_ns exec $first_member -c postgres -- patronictl list 2>/dev/null | grep running | wc -l)
-    if [[ $c_name == "sma-postgres-cluster" ]]; then
-        if [[ $num_patronictl_running -ne 2 ]]; then clusterFailFlag=1; fi
-    else
-        if [[ $num_patronictl_running -ne 3 ]]; then clusterFailFlag=1; fi
-    fi
-
-    if [[ $clusterFailFlag -eq 1 ]]
-    then
+    if [[ $num_patronictl_running -ne $num_of_instances ]]; then 
+        failFlag=1
         if [[ $print_results -eq 1 ]]
         then
-            echo "Error: $c_name instances are not running, shown by patronictl command."
+            echo "$c_name -- $num_patronictl_running instances are not running, shown by patronictl command (fail)"
             kubectl -n $c_ns exec $first_member -c postgres -- patronictl list 2>/dev/null
             echo
-            failFlag=1;
-        else exit 1; fi
-    fi
+	fi
+        continue
+     fi
+     if [[ $print_results -eq 1 ]]
+     then
+  	 echo "$c_name -- Running instances (pass)"
+     fi
 done
 
 if [[ $failFlag -eq 0 ]]; then echo "PASS"; exit 0;
