c0ea7504fc4063a20feda4a3ecb3f9f08d1ae44f
Brad Klein <pingbrad@gmail.com>
2023-03-27 11:10:02 -0600

    Add grace period for etcd statefulsets not old enough for backup (#476)
    Co-authored-by: Brad Klein <bklein@cray.com>

--- goss-testing/scripts/etcd_backups_check.sh
+++ goss-testing/scripts/etcd_backups_check.sh
@@ -32,17 +32,46 @@ do
     esac
 done
 
+#
+# Since clusters subscribed to disaster recovery will have a
+# snapshot taken when the cluster first starts, and the cronjob
+# that pushes snapshots to S3 happens just after the top of the
+# hour, two hours should be enough to have a backup if things are
+# working.  Even though below we check has_recent_backup (below)
+# within a day, that should succeed.
+#
+check_ss_old_enough() {
+    cluster=$1
+    old_enough=0
+    now=$(date +"%s")
+    two_hours_sec=7200
+    create_date=$(kubectl get statefulsets.apps -n services ${cluster}-bitnami-etcd -o jsonpath='{.metadata.creationTimestamp}')
+    create_sec=$(date -d "${create_date}" "+%s" 2>/dev/null)
+    if [[ ! -z $create_sec && $(( $now - $create_sec )) -ge $two_hours_sec ]]; then
+        old_enough=1
+    fi
+    echo $old_enough
+}
+
 error_flag=0
 
 clusters=$(kubectl get statefulsets.apps -A | grep bitnami-etcd | awk '{print $2}')
 for c in $clusters; do
   short_name=$(echo $c | sed s/-bitnami-etcd//g)
   ns=$(kubectl get statefulset -A -o json | jq --arg name ${c} '.items[].metadata | select (.name==$name) | .namespace' | sed 's/\"//g')
+
   dr_setting=$(kubectl get statefulsets.apps -n ${ns} ${c} -o json | jq -r '.spec.template.spec.containers[].env[] | select(."name" == "ETCD_DISASTER_RECOVERY") | .value')
   if [ "$dr_setting" != "yes" ]; then
     echo "$short_name -- not configured for disaster recovery, skipping..."
     continue
   fi
+
+  old_enough=$(check_ss_old_enough $short_name)
+  if [ ! $old_enough ]; then
+    echo "$short_name -- not old enough to expect backups, skipping..."
+    continue
+  fi
+
   #
   # Need to chop off the goofy carriage return in the result from the pod
   #
