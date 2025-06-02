4ebdba7b78bcc1cac1cc367369a7985d9d978556
ANNAPOORNA S <130486454+annapoorna-s-alt@users.noreply.github.com>
2025-06-02 21:17:43 +0530

    CRAYSAT-1996: Allow omitted columns in sat status automated testing (#709)
    * CRAYSAT-1996: Allow omitted columns in sat status automated testing
    
    * Apply suggestions from code review
    
    Fix linting issues
    
    Signed-off-by: Mitch Harding <mitchell.harding@hpe.com>
    
    * Apply suggestions from code review
    
    Address pylint complaints; add some type annotations
    
    Signed-off-by: Mitch Harding <mitchell.harding@hpe.com>
    
    * Address remaining pylint complaints and add remaining missing type annotations
    
    ---------
    
    Signed-off-by: Mitch Harding <mitchell.harding@hpe.com>
    Co-authored-by: Mitch Harding <mitchell.harding@hpe.com>

--- src/csm_testing/tests/sat_functional/status/test_status.py
+++ src/csm_testing/tests/sat_functional/status/test_status.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2024 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2024-2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -27,26 +27,52 @@ Simple tests for the functionality of the `sat status` command.
 import shlex
 import subprocess
 import re
+from typing import List, Tuple
 import unittest
 
-SAT_STATUS_HEADER = ['xname', 'Aliases', 'Type', 'NID', 'State', 'Flag', 'Enabled', 'Arch', 'Class', 'Role', 'SubRole', 'Net Type', 'Desired Config', 'Configuration Status', 'Error Count', 'Boot Status', 'Most Recent BOS Session', 'Most Recent Session Template', 'Most Recent Image']
+SAT_STATUS_HEADER = ['xname', 'Aliases', 'Type', 'NID', 'State', 'Flag', 'Enabled', 'Arch',
+                     'Class', 'Role', 'SubRole', 'Net Type', 'Locked', 'Desired Config',
+                     'Configuration Status', 'Error Count', 'Boot Status',
+                     'Most Recent BOS Session', 'Most Recent Session Template',
+                     'Most Recent Image']
 SAT_FIELDS_HEADER = ['xname', 'Aliases']
-SAT_HSM_FIELDS_HEADER = ['xname', 'Type', 'NID', 'State', 'Flag', 'Enabled', 'Arch', 'Class', 'Role', 'SubRole', 'Net Type']
+SAT_HSM_FIELDS_HEADER = ['xname', 'Type', 'NID', 'State', 'Flag', 'Enabled', 'Arch', 'Class',
+                         'Role', 'SubRole', 'Net Type', 'Locked']
 SAT_SLS_FIELDS_HEADER = ['xname', 'Aliases']
-SAT_HSM_AND_SLS_FIELDS_HEADER = ['xname', 'Aliases', 'Type', 'NID', 'State', 'Flag', 'Enabled', 'Arch', 'Class', 'Role', 'SubRole', 'Net Type']
+SAT_HSM_AND_SLS_FIELDS_HEADER = ['xname', 'Aliases', 'Type', 'NID', 'State', 'Flag', 'Enabled',
+                                 'Arch', 'Class', 'Role', 'SubRole', 'Net Type', 'Locked']
 SAT_CFS_FIELDS_HEADER = ['xname', 'Desired Config', 'Configuration Status', 'Error Count']
-SAT_BOS_FIELDS_HEADER = ['xname', 'Boot Status', 'Most Recent BOS Session', 'Most Recent Session Template', 'Most Recent Image']
+SAT_BOS_FIELDS_HEADER = ['xname', 'Boot Status', 'Most Recent BOS Session',
+                         'Most Recent Session Template', 'Most Recent Image']
 
 
-def get_header(command, num_lines_in_header):
+def get_header(command: str, num_lines_in_header: int) -> Tuple[str, str, str]:
+    """
+    Run the specified command.
+    Return the standard output, standard error, and the header (meaning the first
+    num_lines_in_header lines in the output)
+    """
     # Use stdout and stderr instead of capture_output=True for Python 3.6 compatibility
     proc = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                           check=True)
     status_output = proc.stdout.decode().strip()
+    status_err = proc.stderr.decode().strip()
     status_output_header = '\n'.join(status_output.splitlines()[:num_lines_in_header])
-    return status_output_header
+    return status_output, status_err, status_output_header
 
-def get_column_names_list(input_string):
+def adjust_expected_header(header: List[str], sat_output: str) -> List[str]:
+    """
+    Return a copy of the specified header, removing keys which were omitted in the SAT output
+    """
+    new_header = header.copy()
+    for col in header:
+        info_line = f"INFO: All values for '{col}' are 'MISSING', omitting key."
+        if info_line in sat_output:
+            new_header.remove(col)
+    return new_header
+
+def get_column_names_list(input_string: str) -> List[str]:
+    """ Return the list of column names specified in the header line of the input string """
     # Split the input string into lines
     lines = input_string.strip().split('\n')
 
@@ -62,75 +88,119 @@ def get_column_names_list(input_string):
 class TestStatus(unittest.TestCase):
     """Test the `sat status` command."""
 
-    def test_status_command(self):
+    def test_status_command(self) -> None:
         """Test that `sat status` returns the proper header."""
         command = 'sat status'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_STATUS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_STATUS_HEADER, status_all_output)
 
-    def test_status_fields_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_fields_command(self) -> None:
         """Test that `sat status --fields xname,aliases` returns the proper header."""
         command = 'sat status --fields xname,aliases'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_FIELDS_HEADER, status_all_output)
 
-    def test_status_no_borders_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_no_borders_command(self) -> None:
         """Test that `sat status --no-borders` returns the proper header."""
         command = 'sat status --no-borders'
 
-        status_output_header = get_header(command, 1)
+        status_output, status_err, status_output_header = get_header(command, 1)
         column_names = re.split(r'\s{2,}', status_output_header.strip())
 
-        self.assertEqual(SAT_STATUS_HEADER, column_names)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_STATUS_HEADER, status_all_output)
 
-    def test_status_no_headings_command(self):
+        self.assertEqual(expected_header, column_names)
+
+    def test_status_no_headings_command(self) -> None:
         """Test that `sat status --no-headings` returns the proper header."""
         command = 'sat status --no-headings'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertNotEquals(SAT_STATUS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_STATUS_HEADER, status_all_output)
 
-    def test_status_hsm_fields_command(self):
+        self.assertNotEqual(expected_header, status_output_header)
+
+    def test_status_hsm_fields_command(self) -> None:
         """Test that `sat status --hsm-fields` returns the proper header."""
         command = 'sat status --hsm-fields'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_HSM_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_HSM_FIELDS_HEADER, status_all_output)
 
-    def test_status_sls_fields_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_sls_fields_command(self) -> None:
         """Test that `sat status --sls-fields` returns the proper header."""
         command = 'sat status --sls-fields'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_SLS_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_SLS_FIELDS_HEADER, status_all_output)
 
-    def test_status_hsm_and_sls_fields_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_hsm_and_sls_fields_command(self) -> None:
         """Test that `sat status --sls-fields` returns the proper header."""
         command = 'sat status --hsm-fields --sls-fields'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_HSM_AND_SLS_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_HSM_AND_SLS_FIELDS_HEADER, status_all_output)
 
-    def test_status_cfs_fields_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_cfs_fields_command(self) -> None:
         """Test that `sat status --cfs-fields` returns the proper header."""
         command = 'sat status --cfs-fields'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_CFS_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_CFS_FIELDS_HEADER, status_all_output)
 
-    def test_status_bos_fields_command(self):
+        self.assertEqual(expected_header, status_output_header)
+
+    def test_status_bos_fields_command(self) -> None:
         """Test that `sat status --bos-fields` returns the proper header."""
         command = 'sat status --bos-fields'
 
-        status_output_header = get_column_names_list(get_header(command, 3))
+        status_output, status_err, status_output_header_str = get_header(command, 3)
+        status_output_header = get_column_names_list(status_output_header_str)
 
-        self.assertEqual(SAT_BOS_FIELDS_HEADER, status_output_header)
+        # Combine status_output and status_err for searching INFO lines
+        status_all_output = status_output + '\n' + status_err
+        expected_header = adjust_expected_header(SAT_BOS_FIELDS_HEADER, status_all_output)
+
+        self.assertEqual(expected_header, status_output_header)
