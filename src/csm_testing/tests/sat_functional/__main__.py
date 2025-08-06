09dc39732e10c2c15de9719975fea02e33c7624b
Ryan Haasken <77809410+haasken-hpe@users.noreply.github.com>
2025-08-06 17:28:06 -0500

    CRAYSAT-2022: Dynamically generate SAT Goss tests (#738)
    Add functionality to list available test cases in `sat_functional`
    script. This is then used to dynamically generate the list of tests that
    should be executed by the SAT functional testing Goss suite.
    
    Update `add_local_vars` in `run-ncn-tests.sh` to populate the
    `sat_tests` key in the Goss vars file by executing the main entry point
    of the `sat_functional` package with the new `list` argument and the
    `--format yaml` option to get YAML output.
    
    Remove all the static Goss test files of the form
    `goss-sat-COMMAND.yaml` where `COMMAND` is the command being tested.
    These are no longer needed as all Goss tests cases are being generated.
    
    Test Description:
    
    Unpacked the RPM on rocket and then executed the Goss suite with the
    goss variables file using the `run_goss_tests` shell function from
    `run-ncn-tests.sh`

--- src/csm_testing/tests/sat_functional/__main__.py
+++ src/csm_testing/tests/sat_functional/__main__.py
@@ -43,12 +43,93 @@ becomes:
   `sat_functional version.TestVersion.test_version`
 """
 import argparse
-import sys
-import unittest
+import json
 import os
+import sys
+from typing import List
+import unittest
+
+from yaml import safe_dump
+
+TEST_PACKAGE = 'csm_testing.tests.sat_functional'
 
 
-os.environ['CRAY_FORMAT'] = 'json'
+def get_test_case_names() -> List[str]:
+    """Get a list of all the unittest.TestCase classes in the sat_functional package.
+
+    Returns:
+        The sorted list of the unittest.TestCase class names, relative to the TEST_PACKAGE.
+
+    Raises:
+        RuntimeError: If a discovered test case does not start with the TEST_PACKAGE name.
+    """
+    def get_test_cases_from_suite(suite: unittest.TestSuite) -> List[str]:
+        """Recursively get all test ids from a TestSuite"""
+        test_cases = []
+        for item in suite:
+            if isinstance(item, unittest.TestSuite):
+                test_cases.extend(get_test_cases_from_suite(item))
+            elif isinstance(item, unittest.TestCase):
+                test_cases.append(item.id())
+        return test_cases
+
+    loader = unittest.TestLoader()
+    main_suite = loader.discover(TEST_PACKAGE)
+    full_test_names = get_test_cases_from_suite(main_suite)
+
+    rel_test_names = set()
+    for test_name in full_test_names:
+        if test_name.startswith(TEST_PACKAGE + '.'):
+            # Strip off the TEST_PACKAGE name from the beginning and the test method name from the end
+            rel_test_names.add(test_name[len(TEST_PACKAGE) + 1:].rsplit('.', maxsplit=1)[0])
+        else:
+            raise RuntimeError(f'Discovered test case "{test_name}" does not start with "{TEST_PACKAGE}"')
+
+    return sorted(list(rel_test_names))
+
+
+def list_tests(parsed_args: argparse.Namespace) -> None:
+    """Print the names of all available test cases.
+
+    Args:
+        parsed_args (argparse.Namespace): The parsed arguments from the command line.
+    """
+    test_case_names = get_test_case_names()
+    if parsed_args.format == 'json':
+        print(json.dumps({'sat_tests': test_case_names}))
+    elif parsed_args.format == 'yaml':
+        print(safe_dump({'sat_tests': test_case_names}, default_flow_style=False, sort_keys=False))
+    else:
+        for name in test_case_names:
+            print(name)
+
+
+def run_tests(parsed_args: argparse.Namespace) -> None:
+    """Run the specified tests or all tests if none are specified.
+
+    Args:
+        parsed_args (argparse.Namespace): The parsed arguments from the command line.
+
+    Raises:
+        SystemExit: The exit code is set to 0 if all tests pass, 1 otherwise.
+    """
+    if not parsed_args.tests:
+        # If no tests are specified, run all tests.
+        suite = unittest.TestLoader().discover(TEST_PACKAGE)
+    else:
+        # Load the specified tests.
+        suite = unittest.TestSuite()
+        for test in parsed_args.tests:
+            tests = unittest.TestLoader().loadTestsFromName('.'.join([TEST_PACKAGE, test]))
+            suite.addTests(tests)
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)
+
+    if result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 
 def create_parser() -> argparse.ArgumentParser:
@@ -58,13 +139,29 @@ def create_parser() -> argparse.ArgumentParser:
         The parser object.
     """
     parser = argparse.ArgumentParser(
-        description='Run the specified functional tests for the System Admin Toolkit (SAT).')
-    parser.add_argument(
-        'tests', nargs='+',
+        description='Functional tests for the System Admin Toolkit (SAT).')
+    subparsers = parser.add_subparsers(
+        title='action', description='Action to perform.'
+    )
+
+    list_parser = subparsers.add_parser('list', help='List all available test cases.')
+    list_parser.add_argument(
+        '--format', choices=['text', 'json', 'yaml'], default='text',
+        help='Format to display the list of test cases. The "text" format prints one '
+             'test case name per line, while the "json" format outputs a JSON array of '
+             'test case names. Defaults to "text".'
+    )
+    list_parser.set_defaults(func=list_tests)
+
+    run_parser = subparsers.add_parser('run', help='Run the specified tests.')
+    run_parser.add_argument(
+        'tests', nargs='*',
         help='The names of the tests to run. These should be the names of the '
              'test modules, test classes, or test methods to run, relative to the '
-             'csm_testing.tests.sat_functional package.'
+             f'{TEST_PACKAGE} package. If omitted, all tests will be run.'
     )
+    run_parser.set_defaults(func=run_tests)
+
     return parser
 
 
@@ -75,26 +172,19 @@ def main() -> None:
         SystemExit: The exit code is set to 0 if all tests pass, 1 otherwise.
     """
     parser = create_parser()
-    args = parser.parse_args()
+    parsed_args = parser.parse_args()
 
-    suite = unittest.TestSuite()
+    # Ensure that all `cray` CLI commands default to JSON output. This is even important
+    # for the `list` command, which is used to list available tests because when it discovers
+    # the tests, it runs any `setUpClass` methods in the test cases, which may use the `cray` CLI.
+    os.environ['CRAY_FORMAT'] = 'json'
 
-    for test in args.tests:
-        # Prefix each test name with the path to the sat_functional package
-        tests = unittest.TestLoader().loadTestsFromName(
-            f'csm_testing.tests.sat_functional.{test}'
-        )
-        suite.addTests(tests)
-
-    # Run the test suite with a verbosity of 2 so it will print the name of each
-    # test being executed.
-    runner = unittest.TextTestRunner(verbosity=2)
-    result = runner.run(suite)
-
-    if result.wasSuccessful():
-        sys.exit(0)
-    else:
+    if not hasattr(parsed_args, 'func'):
+        # Needed because required=True is not supported for subparsers in argparse in Python 3.6
+        parser.print_help()
         sys.exit(1)
+
+    parsed_args.func(parsed_args)
 
 
 if __name__ == '__main__':
